4f1f7b578f8074872f40da6dcac1ee5ab17a3394
MelissaCasola <melissacasola@gmail.com>
2024-06-20 15:48:02 +0200

    added isDevMode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
-import { PlayerService } from './services/player.service';
+import { Component, OnInit, isDevMode } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'fca-Barcelona';
+
+  ngOnInit() {
+    if (isDevMode()) {
+      console.log('Development!');
+    } else {
+      console.log('Production!');
+    }
+  }
 
 
 }
